068b88406a94ecce20aca25582b8c2f57e279fc1
ayouberrak <ayouberrak30@gmail.com>
2026-01-05 11:11:54 +0100

    Create Equipe model for team management

---
+++ app/Models/equipe.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Equipe {
+    private ?int $id;
+    private string $nom;
+    private string $logo;
+
+
+    public function __construct(?int $id, string $nom, string $logo)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->logo = $logo;
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getLogo(): string
+    {
+        return $this->logo;
+    }
+
+}

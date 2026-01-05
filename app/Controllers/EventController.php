50ac9c215d94e0d5e576f9b654507d8a44d99de9
ayouberrak <ayouberrak30@gmail.com>
2026-01-05 11:11:54 +0100

    Add EventController for event CRUD operations

---
+++ app/Controllers/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+require_once __DIR__ . '/../Models/Event.php';
+require_once __DIR__ . '/../Repository/EventRepository.php';
+require_once __DIR__ . '/../Models/Equipe.php';
+require_once __DIR__ . '/../Models/Organisateur.php';
+require_once __DIR__ . '/../Services/EventServices.php';
+
+class EventController {
+    
+}
+
+
+// require_once __DIR__ . '/../Views/dashboard/organizer/dashbord.view.php';

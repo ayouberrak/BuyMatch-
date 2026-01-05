d4d55b5406b60263f86587355b8091770c29ad38
ayouberrak <ayouberrak30@gmail.com>
2026-01-05 11:11:54 +0100

    Update Organisateur model with new attributes and methods

--- app/Models/Organisateur.php
+++ app/Models/Organisateur.php
@@ -3,5 +3,11 @@
 
 require_once __DIR__ . '/User.php';
 
-class Organisateur extends User{};
+class Organisateur extends User{
+
+    public function  createEvent() {
+        return $this->getRole() === 'organisateur' && $this->getStatus() === 'actif';
+    
+    }
+};
 

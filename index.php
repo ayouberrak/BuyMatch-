84f1b7c153d8b13d3a0477bdd89cef99a7754b41
ayouberrak <ayouberrak30@gmail.com>
2026-01-05 11:11:54 +0100

    Update index.php to integrate new event modules and routes

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
 
-require_once __DIR__ . '/app/Controllers/SignupController.php';
+// require_once __DIR__ . '/app/Controllers/SignupController.php';

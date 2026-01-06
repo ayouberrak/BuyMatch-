6899b7b0a0695c0a825c47b5b647d201ad889981
ayouberrak <ayouberrak30@gmail.com>
2026-01-06 11:48:10 +0100

    [Repository] Update CommentairesRepository

--- app/Repository/CommentairesRepository.php
+++ app/Repository/CommentairesRepository.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/../Models/Commentaires.php';
 require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../Repository/EventRepository.php';
+require_once __DIR__ . '/../Repository/EquipeRepository.php';
+require_once __DIR__ . '/../Repository/UserRepository.php';
 
 
 class CommentairesRepository{
@@ -76,5 +79,55 @@ class CommentairesRepository{
         }
 
         return $comments;
+    }
+
+    public function getAllComments(): array
+    {
+        $stmt = $this->db->prepare("SELECT c.*, e.*, u.* FROM commentaires c
+                                    JOIN evenements e ON c.event_id = e.id
+                                    INNER JOIN users u ON c.user_id = u.id");
+        $stmt->execute();
+
+        $comments = [];
+        $tousComments = [];
+        $event = new EvnetRepository(); 
+        $equipe = new EquipeRepository();
+        $user = new UserRepository();
+
+        while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $tousComments[] = new Commentaires(
+                $data['id'],
+                $data['event_id'],
+                $data['nom'],
+                $data['image_profil'],
+                $data['titre'],
+                $data['user_id'],
+                $data['contenu'],
+                $data['note'],
+                $data['date_commentaire']
+            );
+
+            $eventDetails = $event->findById($data['event_id']);
+            $equipeA = $equipe->findById($eventDetails->getEquipe1Id());
+            $equipeB = $equipe->findById($eventDetails->getEquipe2Id());
+            $userDetails = $user->findById($data['user_id']);
+
+            $comments[] = [
+                'id' => $data['id'],
+                'match' => $equipeA->getNom() . " vs " . $equipeB->getNom(),
+                'user' => $userDetails,
+                'contenu' => $data['contenu'],
+                'note' => $data['note'],
+                'date_commentaire' => $data['date_commentaire']
+            ];
+        }
+
+        return $comments;
+    }
+
+    public function deleteCommentById(int $id): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM commentaires WHERE id = :id");
+        return $stmt->execute([':id' => $id]);
     }
 }

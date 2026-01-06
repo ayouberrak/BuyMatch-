112eac923747155adcf0966404539db0b97b8fad
ayouberrak <ayouberrak30@gmail.com>
2026-01-06 11:48:10 +0100

    [Controllers] Update AdminControllers

--- app/Controllers/AdminControllers.php
+++ app/Controllers/AdminControllers.php
@@ -1,4 +1,6 @@
 <?php
+
+session_start();
 require_once __DIR__ . '/../Models/User.php';
 require_once __DIR__ . '/../Services/AuthService.php';
 require_once __DIR__ . '/../Repository/UserRepository.php';
@@ -10,18 +12,19 @@ require_once __DIR__ . '/../Models/Organisateur.php';
 require_once __DIR__ . '/../Services/EventServices.php';
 require_once __DIR__ . '/../Services/CommentsService.php';
 require_once __DIR__ . '/../Repository/CommentairesRepository.php';
+require_once __DIR__ . '/../Services/UserServises.php';
 
 class AdminControllers {
-    
-    public function profile()
-    {
-        $user = new UserRepository();
-        return $user->findById($_SESSION['user_id']);
 
+    public function getAllusers()
+    {
+        $userRepo = new UserServises();
+        return $userRepo->getAllUser();
     }
 
 }
-
+$adminController = new AdminControllers();
+$allUsers = $adminController->getAllusers();
 
 class AdminEvents
 {
@@ -38,10 +41,28 @@ class AdminEvents
 }
 
 
-
+AuthMiddleware::checkRole('admin');
 $adminEvents = new AdminEvents();
 $eventsEnatente = $adminEvents->getEventStatusEnatente();
 
+
+class AdminComments
+{
+    private CommentsService $commentsService;
+
+    public function __construct()
+    {
+        $this->commentsService = new CommentsService();
+    }
+
+    public function getAllComments(): array
+    {
+        return $this->commentsService->getAllComments();
+    }
+}
+
+$adminComments = new AdminComments();
+$allComments = $adminComments->getAllComments();
 
 
 

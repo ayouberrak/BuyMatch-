7278ca6819a1ae4324853d31f527fb1d20658bfa
ayouberrak <ayouberrak30@gmail.com>
2026-01-06 15:38:56 +0100

    [Controllers] Update HomePageControllers

--- app/Controllers/homePageControllers.php
+++ app/Controllers/homePageControllers.php
@@ -1,11 +1,30 @@
 <?php
 
+session_start();
 
 
 
 
 
 
+
+
+class LogoutController
+{
+    public function logout()
+    {
+        session_start();
+        session_unset();
+        session_destroy();
+        header("Location: homePageControllers.php");
+        exit();
+    }
+}
+
+$logoutController = new LogoutController();
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+    $logoutController->logout();
+}
 
 
 require_once __DIR__ . '/../Views/homePage.view.php';

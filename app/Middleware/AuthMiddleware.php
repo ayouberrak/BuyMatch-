5999793db2d74713319f2c3744b6689494292b2a
ayouberrak <ayouberrak30@gmail.com>
2026-01-06 11:48:10 +0100

    [Middleware] Update AuthMiddleware

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -5,11 +5,11 @@ class AuthMiddleware {
     
     public static function checkRole(string $role) {
         if (!isset($_SESSION['user_id'])) {
-            header("Location: /login");
+            header("Location: LoginController.php");
             exit();
         }
 
-        if ($_SESSION['user_id']['role'] !== $role) {
+        if ($_SESSION['role'] !== $role) {
             header("HTTP/1.1 403 Forbidden");
             echo "Access Denied!";
             exit();

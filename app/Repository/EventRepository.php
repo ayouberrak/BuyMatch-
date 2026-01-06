5b315732bee22b01cc56e6313b1d6e2795a05a79
ayouberrak <ayouberrak30@gmail.com>
2026-01-06 09:06:33 +0100

    Create EventRepository for event database operations

--- app/Repository/EventRepository.php
+++ app/Repository/EventRepository.php
@@ -50,8 +50,10 @@ class EvnetRepository{
         $stmt->execute([':organisateur_id' => $organisateur]);
 
         $events = [];
+        $equipeRepo = new EquipeRepository();
+
         while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $events[] = new Event(
+            $event = new Event(
                 $data['id'],
                 $data['titre'],
                 $data['mignature'],
@@ -63,9 +65,55 @@ class EvnetRepository{
                 $data['equipe_a_id'],
                 $data['equipe_b_id']
             );
+
+            $equipe1 = $equipeRepo->findById($event->getEquipe1Id());
+            $equipe2 = $equipeRepo->findById($event->getEquipe2Id());
+
+            $events[] = [
+                'event' => $event,
+                'equipe1' => $equipe1,
+                'equipe2' => $equipe2
+            ];
+
+            
         }
 
         return $events;
     }
 
+
+    public function getEventStatusEnatente(): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM evenements WHERE statut = 'en_attente'");
+        $stmt->execute();
+
+        $events = [];
+        $equipeRepo = new EquipeRepository();
+
+        while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $event = new Event(
+                $data['id'],
+                $data['titre'],
+                $data['mignature'],
+                $data['date_event'],
+                $data['lieu'],
+                $data['statut'],
+                $data['organisateur_id'],
+                $data['note_moyenne'],
+                $data['equipe_a_id'],
+                $data['equipe_b_id']
+            );
+
+            $equipe1 = $equipeRepo->findById($event->getEquipe1Id());
+            $equipe2 = $equipeRepo->findById($event->getEquipe2Id());
+
+            $events[] = [
+                'event' => $event,
+                'equipe1' => $equipe1,
+                'equipe2' => $equipe2
+            ];
+        }
+
+        return $events;
+    }
 }

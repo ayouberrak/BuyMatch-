43b4352406ee441b7530b66fed547d8b33336372
ayouberrak <ayouberrak30@gmail.com>
2026-01-07 11:06:40 +0100

    [Models] Update Reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -5,15 +5,17 @@ class Reservation {
     private int $event_id;
     private int $user_id;
     private string $date_reservation;
-    private int $nombre_tickets;
+    private int $prix_total;
+    private int $quantite;
 
-    public function __construct(?int $id, int $event_id, int $user_id, string $date_reservation, int $nombre_tickets)
+    public function __construct(?int $id, int $event_id, int $user_id, string $date_reservation, int $quantite, int $prix_total)
     {
         $this->id = $id;
         $this->event_id = $event_id;
         $this->user_id = $user_id;
         $this->date_reservation = $date_reservation;
-        $this->nombre_tickets = $nombre_tickets;
+        $this->quantite = $quantite;
+        $this->prix_total = $prix_total;
     }
 
     public function getId(): ?int
@@ -36,8 +38,13 @@ class Reservation {
         return $this->date_reservation;
     }
 
-    public function getNombreTickets(): int
+    public function getPrixTotal(): int
     {
-        return $this->nombre_tickets;
+        return $this->prix_total;
     }
+    public function getQuantite(): int
+    {
+        return $this->quantite;
+    }
+    
 }

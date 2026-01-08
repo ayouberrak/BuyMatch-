05e5ce4cbd74257f8b3b62233fe04df32f2a5103
ayouberrak <ayouberrak30@gmail.com>
2026-01-08 12:01:54 +0100

    [Controllers] Update DetailsController

--- app/Controllers/DetailsController.php
+++ app/Controllers/DetailsController.php
@@ -1,7 +1,11 @@
 <?php
-
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../Services/EventServices.php';
+require_once __DIR__ . '/../Middleware/AuthMiddleware.php';
 
 
 class DetailsController
@@ -18,11 +22,28 @@ class DetailsController
         return $this->eventServices->getEventById($eventId);
     }
 }
+
+if(AuthMiddleware::checkRole('acheteur')){
+    header("Location: LoginController.php");
+    exit();
+
+}
+
 $detailsController = new DetailsController();
-$eventId = intval($_GET['id']);
+$eventId = $_GET['id'];
 $event = $detailsController->getEventDetails($eventId);
 
+$eventDetails = $event['event'];
+$equipe1Data = $event['equipe1'];
+$equipe2Data = $event['equipe2'];
 
+// print_r($event);
+// print_r($equipe1Data);
+// print_r($equipe2Data);
+// echo $eventId;
+
+
+require_once __DIR__ . '/CategorieControllers.php';
 
 
 require_once __DIR__ . '/../Views/matches/deitails.php';
